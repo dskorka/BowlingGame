da596227305641ea8a826e515bbb34c249ea23c3
skorkad <skorkad>
2016-10-06 10:00:44 +0200

    refactoring

--- src/main/java/pl/kata/bowlinggame/finder/SortParameter.java
+++ src/main/java/pl/kata/bowlinggame/finder/SortParameter.java
@@ -5,21 +5,13 @@ public enum SortParameter {
 	ID(1),
 	TIME_GAME(3);
 	
-	String columnName;
 	int columnNumber;
 	
 	private SortParameter(int columnNumber){
 		this.columnNumber = columnNumber;
 	}
 	
-	public String getColumnName(){
-		return columnName;
-	}
-	
 	public int getColumnNumber(){
 		return columnNumber;
 	}
-	
-
-	
 }
